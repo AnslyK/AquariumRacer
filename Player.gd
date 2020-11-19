e01d49197c347c5f5d82a8a5b8adc74e1eb3145d
Axel <axel.murat@laposte.net>
2020-11-19 13:54:24 +0100

    Playerv1
    ressource + scene + unfinished script

---
+++ Player.gd
@@ -0,0 +1,42 @@
+extends Area2D
+
+signal hit
+
+export var speed = 400  # How fast the player will move (pixels/sec).
+var screen_size  # Size of the game window.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+		$AnimatedSprite.animation = "right"
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+		$AnimatedSprite.animation = "left"
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		position += velocity * delta
+		position.x = clamp(position.x, 0, screen_size.x)
+		position.y = clamp(position.y, 0, screen_size.y)
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+
+
+func _on_Player_body_entered(body):
+	emit_signal("hit")
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+	
