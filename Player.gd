9ba3c412ad750ce47825c113dc4ccd8316af6b27
unknown <Kaotico06@gmail.com>
2020-11-20 09:04:04 +0100

    (player) fix invisible player

--- Player.gd
+++ Player.gd
@@ -9,6 +9,7 @@ var immune = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	$AnimatedSprite.animation = "right"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
